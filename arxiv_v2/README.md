b068d6108001e00b69a2ea83ce2de4346eba0cd4
someshs <someshs@adobe.com>
2024-12-23 06:30:38 +0530

    v3

--- arxiv_v2/README.md
+++
@@ -1,3 +0,0 @@
-# Personal Website
-
-The implementation is an enhanced version of academic pages (Jekyll Template) Check them out -> [Academic Pages](https://github.com/academicpages/academicpages.github.io)
---
+++ utils.js
@@ -0,0 +1,29 @@
+function createProjectElement(id, project){
+    /* Create an element in Project
+    tag: some tags
+    title: title of the project or publications
+    paper_url: link to the paper
+    authors: authors
+    conference: publication venue
+    image (optional): directory to project image
+    others (optional): anything else
+    */
+    if (project.tag == null)
+        project.tag = id;
+    if (project.others == null)
+        project.others = "";
+
+    html_img = `<img src='${project.image}' style="max-width: 300px"></div></div>`
+    html_txt = `<p>
+      <a href="${project.paper_url}"><papertitle>${project.title}</papertitle></a>
+      <br>
+      <br>
+      ${project.authors}
+      <br>
+      <em>${project.conference}</em>
+      <br>
+      ${project.others}`
+
+    document.getElementById(id + "-img").innerHTML = html_img;
+    document.getElementById(id + "-txt").innerHTML = html_txt;
+}
